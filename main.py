45bb00732fed7b41dfde290720d09e8d8599c0af
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-26 21:46:20 +1000

    Logic for sending email finished completely and email sent successfully

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 import pandas as pd
 import datetime as dt
 import random as rd
+import smtplib
 
 # 1. Update the birthdays.csv
-
 # READ CSV
 bday_df = pd.read_csv("birthdays.csv")
 bday_data = bday_df.to_dict(orient='records')
-# print(bday_data_dict)
 
 # 2. Check if today matches a birthday in the birthdays.csv
 now = dt.datetime.now()
@@ -20,11 +19,28 @@ date_today = now.date()
 for bday in bday_data:
     if bday['month'] == month and bday['day'] == day:
         random_letter = f"./letter_templates/letter_{rd.randint(1,3)}.txt"
-        
-        with open(random_letter, 'r') as letter_file:
-            letter = letter_file.read()
-            new_letter = letter.replace("[NAME]", bday['name'])
+        bday_person = bday
+
+with open(random_letter, 'r') as letter_file:
+    letter = letter_file.read()
+    new_letter = letter.replace("[NAME]", bday_person['name'])
             
-
 # 4. Send the letter generated in step 3 to that person's email address.
+my_email = "jordansyariefjs@gmail.com"
+password = "vtoaypkicpoumgyf"
 
+def smtp_sendmail():
+    subject = f"Happy Birthday {bday_person['name']}"
+    content = new_letter
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, 
+                                to_addrs=my_email, 
+                                msg=f"Subject:{subject}\n\n{content}")
+    except:
+        print("connection error.")
+    else:
+        print("E-Mail sent successfully.")
+
+smtp_sendmail()
